67d271098a661f16c2ba66aac266bbc27c3ccfda
Ethan Sagin <ethansagin@gmail.com>
2019-05-22 14:43:05 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,5 +1,6 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+<<<<<<< HEAD
 require_relative './fancy_dance.rb'
 
 class Dancer
@@ -13,4 +14,12 @@ class Dancer
   end
   
   
+=======
+
+class Dancer
+  
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+  
+>>>>>>> b545bd1649b1ab4983f248331d1200fb782beff4
 end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,5 +1,6 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+<<<<<<< HEAD
 require_relative './fancy_dance.rb'
 
 
@@ -7,9 +8,22 @@ class Kid
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 
+=======
+
+class Kid
+  
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+ 
+>>>>>>> b545bd1649b1ab4983f248331d1200fb782beff4
   attr_accessor :name
  
   def initialize(name)
     @name = name
   end
+<<<<<<< HEAD
+=======
+  
+ 
+>>>>>>> b545bd1649b1ab4983f248331d1200fb782beff4
 end
